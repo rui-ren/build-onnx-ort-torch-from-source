f2a2051df4cacee19231ad21da2fd73984299f99
rui-ren <ruiren1225@gmail.com>
2023-11-07 15:05:27 -0800

    build Training ORT

--- build_ort_training_from_source.sh
+++ build_ort_training_from_source.sh
@@ -4,37 +4,16 @@ set -eoux pipefail
 
 pip uninstall onnxruntime-training -y
 
-export CUDA_VERSION=11.6
+export CUDA_VERSION=11.8
 
 # set the package path
-export TRT_HOME=/usr/lib/x86_64-linux-gnu
 export CUDA_HOME=/usr/local/cuda-$CUDA_VERSION
 export CUDNN_HOME=/usr/local/cuda-$CUDA_VERSION
 export CUDACXX=/usr/local/cuda-$CUDA_VERSION/bin/nvcc
-export PATH=/usr/lib/x86_64-linux-gnu/openmpi/bin:$PATH
-export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/openmpi/lib:$LD_LIBRARY_PATH
-export MPI_CXX_INCLUDE_PATH=/usr/lib/x86_64-linux-gnu/openmpi/include/
-export CMAKE_CUDA_ARCHITECTURES="37;50;52;60;61;70;75;80"
-
-# Install TensorRT
-v="8.4.1-1+cuda11.6"
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-apt-get update
-sudo apt-get install -y libnvinfer8=${v} \
-    libnvonnxparsers8=${v} \
-    libnvparsers8=${v} \
-    libnvinfer-plugin8=${v} \
-    libnvinfer-dev=${v} \
-    libnvonnxparsers-dev=${v} \
-    libnvparsers-dev=${v} \
-    libnvinfer-plugin-dev=${v} \
-    python3-libnvinfer=${v} \
-    libnvinfer-samples=${v}
-
-# Compile trtexec
-cd /usr/src/tensorrt/samples/trtexec && make
 
 # save the package to onnxruntime
+
+mkdir -p acpt
 cd /acpt
 
 # clone the onnxruntime repo
@@ -42,26 +21,23 @@ git clone https://github.com/microsoft/onnxruntime
 
 cd onnxruntime
 
-# install the stable version of onnxruntime
-git checkout v${ort_version}
-
-# For onnxruntime >= 1.14.1, we need to add --allow-as-root
-
 /bin/sh build.sh --parallel \
         --build_shared_lib \
-        --cuda_home /usr/local/cuda \
-        --cudnn_home /usr/lib/x86_64-linux-gnu/ \
-        --use_tensorrt \
-        --tensorrt_home /usr/lib/x86_64-linux-gnu/ \
+        --enable_training \
+        --cuda_version ${CUDA_VERSION} \
+        --cuda_home ${CUDA_HOME} \
+        --cudnn_home ${CUDNN_HOME} \
         --config RelWithDebInfo \
         --build_wheel \
+        --enable_nvtx_profile \
         --skip_tests \
-        --skip_submodule_sync \
-        --cmake_extra_defines '"CMAKE_CUDA_ARCHITECTURES='${CMAKE_CUDA_ARCHITECTURES}'"'
+        --allow_running_as_root
 
 # Install the onnxruntime whe
 python -m pip install --no-index --no-deps ./build/Linux/RelWithDebInfo/dist/*.whl
 cd ..
+# remove acpt
+rm -rf ./acpt
 
 # enable ORTModule
 python -m onnxruntime.training.ortmodule.torch_cpp_extensions.install
